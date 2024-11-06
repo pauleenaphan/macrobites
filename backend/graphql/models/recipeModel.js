9d09f9eadb3c7595e90fcd8f3da4fe241312143d
pauleenaphan <pauleena2002@gmail.com>
2024-11-05 17:18:19 -0800

    added models

---
+++ backend/graphql/models/recipeModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const recipeSchema = new Recipe({
+    name: { type: String, required: true },
+    body: { type: String, required: true },
+    coverPhoto: { type: String },
+    macros: {
+        protein: Number,
+        fat: Number,
+        carbs: Number,
+        calories: Number
+    },
+    readTime: { type: String, required: true },
+    cookTime: { type: String, required: true },
+    images: { type: [String] }
+})
+
+modules.exports = mongoose.model("Recipe", recipeSchema);
